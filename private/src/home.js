6482bcd9c1c2ece19112994f393b5dd3e910add0
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-01-29 22:03:36 -0300

    get add .

--- private/src/home.js
+++ private/src/home.js
@@ -1,9 +1,40 @@
 const home = ()=>{
-    // let calculator = document.getElementById('calculator')
-    let calcs = ['Multiply', 'Share', 'Subtration', 'Add']
-    for(let i = 0;i<= calcs.length;i++){
-        console.log(calcs[i])
+    let calcs = document.getElementById('calcs')
+    let btn = document.getElementById('btn')
+    let calcS = ['Multiply', 'Share', 'Subtration', 'Add']
+    if(btn){
+        btn.onclick = ()=>{
+            calcs.innerHTML=`
+            <div class='calc'>
+                <button id='btn1'>Multiply</button>
+            </div>
+            <div class='calc'>
+                <button id='btn1'>Share</button>
+            </div>
+            <div class='calc'>
+                <button id='btn1'>Subtration</button>
+            </div>
+            <div class='calc'>
+                <button id='btn1'>Add</button>
+            </div>
+            <div id='result'></div>
+            `
+            let btn1 = document.getElementById('btn1')
+            let result = document.getElementById('result')
+            let n1 = document.getElementById('n1')
+            let n2 = document.getElementById('n2')
+            let mult = parseInt(n1.value)*parseInt(n2.value)
+            let shar = parseInt(n1.value)/parseInt(n2.value)
+            let subt = parseInt(n1.value)-parseInt(n2.value)
+            let add = parseInt(n1.value)+parseInt(n2.value)
+            if(btn1){
+                btn1.onclick = ()=>{
+                    result.innerHTML=`<h4>${mult}</h4>`
+                }
+            }
+        }
     }
 }
+
 
 export { home }
